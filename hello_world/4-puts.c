7f73ba7354700310ef12660446b23a0a1cb80d65
root <blitzcold0819@duck.com>
2024-02-24 23:46:24 +0000

    puts B

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,11 +3,13 @@
 
 /**
  * main - prints "Programming is like building a multilingual 
- * puzzle
+ * puzzle, folloed by a newline.
  * Return: Always 0.
  */
 
 int main(void)
+
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

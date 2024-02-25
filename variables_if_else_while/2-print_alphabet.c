eb056e00ef21b146aa32a07d9555a0556a7e9049
root <blitzcold0819@duck.com>
2024-02-25 03:40:10 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text according number
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}

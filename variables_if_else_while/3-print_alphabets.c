3d1d649e3d50cd07a900bf863c782f734840cdb4
root <blitzcold0819@duck.com>
2024-02-25 04:04:54 +0000

    alphabet +

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a text according number
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'a'; low <= 'a'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'B'; low++)
+	putchar(low);
+	putchar('\n');
+
+        return (0);
+
+}

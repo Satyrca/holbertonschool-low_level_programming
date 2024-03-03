29a531df372fdeb603fdba8329a65c360e739908
Satyrca <blitzcold0819@duck.com>
2024-03-03 04:15:17 +0000

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,6 +3,7 @@
 /**
  * mul - Multiply two integers
  * @x: The first number to multiply
+ * @y: The second number to multiply with the first number
  * Return: The result of multiplication between x param and y param
  */
 

add616495f61d4449453258798d5c68cd34ee09d
Satyrca <blitzcold0819@duck.com>
2024-03-03 04:23:57 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - Print the number since 0 up to 9
+ * print_numbers - Print the numbers since 0 up to 9
  * Return: The numbers since 0 up to 9
  */
 
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		_putchar(x + '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
